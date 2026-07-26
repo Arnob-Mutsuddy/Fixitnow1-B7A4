8fec246829d71e8f281311723259256c1feffaac
Arnob Mutsuddy <arnobmutsuddy11@gamil.com>
2026-07-26 13:44:43 +0600

    add global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express, { Application, Request, Response, NextFunction } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import notFound from "./middleware/notFound";
+import { globalErrorHandler } from "./middleware/globalErrorHandler";
 
 const app: Application = express();
 
@@ -21,25 +23,27 @@ app.get("/", (req: Request, res: Response) => {
 // Routes will be mounted here later
 // app.use("/api", router);
 
+app.use(notFound);
+app.use(globalErrorHandler);
 // 404 handler
-app.use((req: Request, res: Response) => {
-  res.status(404).json({
-    success: false,
-    message: "Route not found",
-    errorDetails: {
-      path: req.originalUrl,
-      method: req.method,
-    },
-  });
-});
+// app.use((req: Request, res: Response) => {
+//   res.status(404).json({
+//     success: false,
+//     message: "Route not found",
+//     errorDetails: {
+//       path: req.originalUrl,
+//       method: req.method,
+//     },
+//   });
+// });
 
-// Global error handler
-app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-  res.status(err.statusCode || 500).json({
-    success: false,
-    message: err.message || "Something went wrong",
-    errorDetails: err,
-  });
-});
+// // Global error handler
+// app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+//   res.status(err.statusCode || 500).json({
+//     success: false,
+//     message: err.message || "Something went wrong",
+//     errorDetails: err,
+//   });
+// });
 
 export default app;
