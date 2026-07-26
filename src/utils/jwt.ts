44056abf0a2fe26cdf568d608784327319dca9ad
Arnob Mutsuddy <arnobmutsuddy11@gamil.com>
2026-07-26 21:46:36 +0600

    add auth service to handling jwt , resisterUser and logingUser

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -3,7 +3,7 @@ import jwt, { JwtPayload, SignOptions } from "jsonwebtoken";
 const createToken = (
   payload: JwtPayload,
   secret: string,
-  expiresIn: SignOptions["expiresIn"]
+  expiresIn: SignOptions
 ) => {
   const token = jwt.sign(payload, secret, { expiresIn } as SignOptions);
   return token;
