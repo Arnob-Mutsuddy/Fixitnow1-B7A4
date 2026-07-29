757913b36a79406b473adfcc65db2f34a215a05f
Arnob Mutsuddy <arnobmutsuddy11@gamil.com>
2026-07-29 21:43:34 +0600

    add review route

---
+++ src/module/review/review.route.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { reviewController } from "./review.controller.js"
+import { auth } from "../../middleware/auth.js";
+import { Role } from "../../../prisma/generated/prisma/enums.js";
+
+const router = Router();
+
+router.post("/", auth(Role.CUSTOMER), reviewController.createReview);
+router.get("/technician/:technicianId", reviewController.getReviewsByTechnician);
+
+export const reviewRoutes = router;
